c238741565104ece2e3ae649e728cb9e4c7fd0a5
DuyThong28 <duythong28112004@gmail.com>
2024-04-15 17:32:16 +0700

    patient api

---
+++ src/main/java/pmc/private_medical_clinic/Dto/PatientDto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pmc.private_medical_clinic.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author duyth
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientDto {
+   private String fullName;
+   private String gender;
+   private Integer birthYear;
+   private String phoneNumber;
+   private String address;
+}
---
+++ src/main/java/pmc/private_medical_clinic/Entity/Patient.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pmc.private_medical_clinic.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author duyth
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String fullName;
+    private String gender;
+    private Integer birthYear;
+    private String address;
+    private String phoneNumber;
+}
