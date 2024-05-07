7b96c847e93c872ae1ccbf90557c8702d0ad6757
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-05-07 18:17:35 +0700

    login with gg and reset pass

--- src/main/java/pmc/private_medical_clinic/Configs/WebSecurityConfig.java
+++ src/main/java/pmc/private_medical_clinic/Configs/WebSecurityConfig.java
@@ -2,22 +2,29 @@ package pmc.private_medical_clinic.Configs;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.OAuth2ResourceServerDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -28,12 +35,15 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 @Configuration
 @EnableMethodSecurity
+@Order(1)
 public class WebSecurityConfig {
 
     @Autowired
     private AuthFilterService authFilterService;
     @Autowired
     private AuthenticationProvider authenticationProvider;
+    @Autowired
+    private Oauth2Config oauth2Config;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -44,6 +54,10 @@ public class WebSecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated())
+                .oauth2Login(oath2 -> {
+                    oath2.successHandler(oauth2Config);
+                    oath2.failureHandler(new SimpleUrlAuthenticationFailureHandler("/login"));
+                })
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
--- src/main/java/pmc/private_medical_clinic/Controllers/Login_ResetPass.java
+++ src/main/java/pmc/private_medical_clinic/Controllers/Login_ResetPass.java
@@ -95,7 +95,6 @@ public class Login_ResetPass {
         ResponeInfo<AuthResponse> responeInfo = new ResponeInfo<>();
         try {
             AuthResponse refreshAccToken = new AuthResponse();
-
             RefreshToken refreshToken = refreshTokenService.verifyRefreshToken(refreshTokenDto.getRefreshToken());
             User user = refreshToken.getUser();
             String accessToken = jwtService.generateToken(user);
